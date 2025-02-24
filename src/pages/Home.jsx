0d1e156c39b168047fe21519de5e6311adb6ea6a
Suman <sumaka37@gmail.com>
2025-02-25 00:20:35 +0545

    added two components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
+import OurBrand from '../components/OurBrand'
+import TopStation from '../components/TopStation'
 
 const Home = () => {
     return (
         <div>
             <Header />
+            <OurBrand />
+            <TopStation />
         </div>
     )
 }
