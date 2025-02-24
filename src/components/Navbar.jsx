edf01ca278326f1378f7d6be5ecf578f9af3cd1c
Suman <sumaka37@gmail.com>
2025-02-24 22:26:07 +0545

    complete navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,60 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { assets } from '../assets/assets_frontend/assets.js'; // Corrected import path
+import React, { useState } from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { assets } from '../assets/assets_frontend/assets'
 
 const Navbar = () => {
+    const navigate = useNavigate()
+    const [showMenu, setShowMenu] = useState(false)
+    const [token, setToken] = useState(true)
     return (
-        <div>
-            <img src={assets.logo} alt="Logo" />
+        <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400 '>
+            <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
+            <ul className='hidden md:flex items-start gap-5 font-medium'>
+                <NavLink to='/'>
+                    <li className='py-1'>HOME</li>
+                    <hr className='border-none outline-none h-0.5 bg-green-500 w-3/5 m-auto hidden ' />
+                </NavLink>
+                <NavLink to='/map'>
+                    <li className='py-1'>MAP</li>
+                    <hr className='border-none outline-none h-0.5 bg-green-500 w-3/5 m-auto hidden' />
+                </NavLink>
+                <NavLink to='/station'>
+                    <li className='py-1'>STATION</li>
+                    <hr className='border-none outline-none h-0.5 bg-green-500 w-3/5 m-auto hidden' />
+                </NavLink>
+                <NavLink to='/about'>
+                    <li className='py-1'>ABOUT US</li>
+                    <hr className='border-none outline-none h-0.5 bg-green-500 w-3/5 m-auto hidden' />
+                </NavLink>
+                <NavLink to='/contact'>
+                    <li className='py-1'>CONTACT</li>
+                    <hr className='border-none outline-none h-0.5 bg-green-500 w-3/5 m-auto hidden' />
+                </NavLink>
 
-            <ul>
-                <NavLink to="/">
-                    <li>Home</li>
-                    <hr />
-                </NavLink>
-                <NavLink to="/station">
-                    <li>Station</li>
-                    <hr />
-                </NavLink>
-                <NavLink to="/map">
-                    <li>Map</li>
-                    <hr />
-                </NavLink>
-                <NavLink to="/about">
-                    <li>About</li>
-                    <hr />
-                </NavLink>
-                <NavLink to="/contact">
-                    <li>Contact</li>
-                    <hr />
-                </NavLink>
             </ul>
+            <div className='flex items-center gap-4'>
+                {
+                    token
+                        ? <div className='flex items-center gap-2 cursor-pointer group relative' >
+                            <img className='w-8 rounded-full' src={assets.profile_pic} alt="" />
+                            <img className='w-2.5' src={assets.dropdown_icon} alt="" />
+                            <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+                                <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                                    <p onClick={() => navigate('/myprofile')} className='hover:text-black cursor-point'>My Profile</p>
+                                    <p onClick={() => navigate('/my-appointments')} className='hover:text-black cursor-point'>My Appointment</p>
+                                    <p onClick={() => setToken(false)} className='hover:text-black cursor-point'>Logout</p>
+                                </div>
+                            </div>
+                        </div>
+                        : <button onClick={() => navigate('/login')} className='bg-green-400 text-white px-8 py-3 rounded-full font-light hidden md:block'>
+                            Create Account
+                        </button>
+                }
 
-            <div>
-                <button>Create Account</button>
             </div>
+
         </div>
-    );
-};
+    )
+}
 
 export default Navbar;
