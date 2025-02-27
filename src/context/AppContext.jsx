2a82f1ab6e541d460be09d73e3e3d1c1b5b75be8
Suman <sumaka37@gmail.com>
2025-02-27 20:51:30 +0545

    added some functions

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import { createContext, useContext, useEffect, useState } from "react";
+import { toast } from 'react-toastify';
 
 export const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
     const [evStations, setEvStations] = useState([]);
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
-    const [token, setToken] = useState('')
+    const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
+    const [userData, setUserData] = useState(false)
 
     // ✅ Fetch Stations from Backend
     const getStationData = async () => {
@@ -24,17 +26,48 @@ const AppContextProvider = ({ children }) => {
         }
     };
 
+
+    const loadUserProfileData = async () => {
+
+        try {
+
+            const { data } = await axios.get(backendUrl + '/api/user/get-profile', { headers: { token } })
+            if (data.success) {
+                setUserData(data.userData)
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+
+    }
+
     // ✅ Auto-fetch station data on mount
     useEffect(() => {
         getStationData();
     }, []);
+
+    useEffect(() => {
+
+        if (token) {
+            loadUserProfileData()
+        } else {
+            setUserData(false)
+        }
+
+    }, [token])
 
     // ✅ Provide context values
     const value = {
         evStations,
         getStationData,
         token, setToken,
-        backendUrl
+        backendUrl,
+        userData, setUserData,
+        loadUserProfileData,
 
     };
 
